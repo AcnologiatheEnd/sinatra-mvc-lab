7f54b0fe56704fff66d5090696e004d70a2b1645
Winston  Chung <18.chung.winston@ctkprep.org>
2019-12-25 13:59:49 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,64 +1,23 @@
 class PigLatinizer
-def piglatinize(str)
-    # Vowels to consider
-    vowels = ["a", "e", "i", "o", "u","A","E","I","O","U"]
-    
-    # Special cases to consider
-    two_letter_consonants = ["ch", "sh", "qu", "th", "br"]
-    three_letter_consonants = ["thr", "sch", "squ"]
+def piglatinize(word)
 
-    # Seperate each word from the phrase given
-    words = str.split(" ")
-    
-    #Location for processed words
+    @word = word
+    @words = user_input_text.split(" ")
     result = []
-    
-    
-    
-    words.each do |word|
-        # Words that start with a vowels
-        return word if %w[and an in].include?(word)
-        if vowels.include? word[0]
-            result.push word << 'way'
-            
-        # Words that start with a consonant
-        else
-            # Check for special consonants
-            if three_letter_consonants.include? word[0] + word[1] + word[2]
-                # Slice off first three letters
-                first_three_letters = word.slice!(0,3)
-                
-                # Add letters to end of word with 'ay'
-                result.push word << first_three_letters << 'ay'
-                
-            elsif  two_letter_consonants.include? word[0] + word[1]
-                # Slice off first two letters
-                first_two_letters = word.slice!(0,2)
-                
-                # Add the letters to end of word with 'ay'
-                result.push word << first_two_letters << 'ay'
-                
-            else
-                # Slice off first letter...
-                first_letter = word.slice!(0)
-                
-                # Add first letter to end of word with 'ay'
-                result.push word << first_letter << 'ay'
-            end #End of special consonant check
-    
-        end #End of vowel check
-        
-        
-        
-    end #End of words.each
-    
-    
-    
-    #Present the processed words as a single string
-    result.join(" ")
 
-end #End of translate function
+    @words.each do |word|
+      if word.start_with?(/[AEIOUaeiou]/)
+        result << "#{word}way"
+      elsif word.start_with?(/[sS][pt]r{1}/)
+        result << "#{word[3..-1]}#{word[0..2]}ay"
+      elsif word.start_with?(/\w[rhltk]/)
+        result << "#{word[2..-1]}#{word[0..1]}ay"
+      else
+        result << "#{word[1..-1]}#{word[0]}ay"
+      end
+    end
+
+    @piglatinized = result.join(" ")
+
+  end 
 end
-
-
-
