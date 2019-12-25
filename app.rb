eac52494fd531fa4aceb317743110f9d37eb6b5d
Winston  Chung <18.chung.winston@ctkprep.org>
2019-12-25 14:06:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input
+  end
+  
+  post '/piglatinize' do 
+    @word = params[:user_phrase]
+    @result = PigLatinizer.new.piglatinize(@word)
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,22 @@
+class PigLatinizer
+def piglatinize(word)
+
+    @words = word.split(" ")
+    result = []
+
+    @words.each do |word|
+      if word.start_with?(/[AEIOUaeiou]/)
+        result << "#{word}way"
+      elsif word.start_with?(/[sS][pt]r{1}/)
+        result << "#{word[3..-1]}#{word[0..2]}ay"
+      elsif word.start_with?(/\w[rhltk]/)
+        result << "#{word[2..-1]}#{word[0..1]}ay"
+      else
+        result << "#{word[1..-1]}#{word[0]}ay"
+      end
+    end
+
+    @piglatinized = result.join(" ")
+
+  end 
+end
